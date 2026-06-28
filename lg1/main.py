02d5c4eaa9cc153fce82df7c0727bd706741e167
rayansyed95 <114736482+rayansyed95@users.noreply.github.com>
2026-06-28 13:56:13 +0100

    main

---
+++ lg1/main.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+from langchain.tools import tool
+from langchain.chat_models import init_chat_model
+
+
+load_dotenv()
+
+model = init_chat_model(
+    "gpt-5.5",
+    temperature=0
+)
+
+
+# Define tools
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a * b
+
+
+@tool
+def add(a: int, b: int) -> int:
+    """Adds `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a + b
+
+
+@tool
+def divide(a: int, b: int) -> float:
+    """Divide `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a / b
+
+
+# Augment the LLM with tools
+tools = [add, multiply, divide]
+tools_by_name = {tool.name: tool for tool in tools}
+model_with_tools = model.bind_tools(tools)
+
+print("Hello")
+
