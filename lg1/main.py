a2133b6aad7c15a4b4b85582d68dbfdd6b2c528a
rayansyed95 <114736482+rayansyed95@users.noreply.github.com>
2026-06-28 14:14:51 +0100

    created tool node

--- lg1/main.py
+++ lg1/main.py
@@ -2,8 +2,18 @@ from dotenv import load_dotenv
 from langchain.tools import tool
 from langchain.chat_models import init_chat_model
 
+from langchain.messages import AnyMessage
+from typing_extensions import TypedDict, Annotated
+import operator
+
+from langchain.messages import SystemMessage
+from langchain.messages import ToolMessage
 
 load_dotenv()
+
+class MessagesState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+    llm_calls: int
 
 model = init_chat_model(
     "gpt-5.5",
@@ -50,5 +60,31 @@ tools = [add, multiply, divide]
 tools_by_name = {tool.name: tool for tool in tools}
 model_with_tools = model.bind_tools(tools)
 
-print("Hello")
 
+def llm_call(state: dict):
+    """LLM decides whether to call a tool or not"""
+
+    return {
+        "messages": [
+            model_with_tools.invoke(
+                [
+                    SystemMessage(
+                        content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+                    )
+                ]
+                + state["messages"]
+            )
+        ],
+        "llm_calls": state.get('llm_calls', 0) + 1
+    }
+
+
+def tool_node(state: dict):
+    """Performs the tool call"""
+
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool = tools_by_name[tool_call["name"]]
+        observation = tool.invoke(tool_call["args"])
+        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
